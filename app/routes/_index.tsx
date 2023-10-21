70127a4115fa7e88f584c7a181efe93253fe3f6a
Rolands Kundzins <rolis.kungs@inbox.lv>
2023-10-21 09:39:41 +0300

    #6 Designing basic google maps api

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,29 +12,17 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <h2>Pagaidām pievienoju šādi linkus  uz visām lapām, lai var visas lapas apskatīties</h2>
+      <h3>Drošvien, ka ka pirmajai lapai vajadzēs būt signin/login, nevis šajai.</h3>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to="/login">Login</Link>;
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <Link to="/signin">Signin</Link>;
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to="/navigate">Navigate</Link>;
         </li>
       </ul>
     </div>
--- app/routes/navigate.tsx
+++ app/routes/navigate.tsx
@@ -1,16 +1,15 @@
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import "./css/navigate.css"
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyAm6YQArZ7RCT3WgjV_GP7g73Pm8kHMBxg",
+    googleMapsApiKey: "AIzaSyAm6YQArZ7RCT3WgjV_GP7g73Pm8kHMBxg", //remove this later (.env)
   });
 
   if (!isLoaded) return <div>Loading...</div>;
   return(
     <div>
-      <h1>Hi</h1>
       <Map />
     </div>
   )
@@ -18,11 +17,18 @@ export default function Home() {
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+  const center = useMemo(() => ({ lat: 56.92, lng: 24.12 }), []);
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
+    <GoogleMap zoom={11} center={center} mapContainerClassName="map-container"
+    options={{
+      zoomControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
+      fullscreenControl: false,
+
+    }}>
+      <MarkerF position={center} />
     </GoogleMap>
   );
 }
